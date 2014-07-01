56cc767115c6cf8679a6db97c5085c36898df35d
Ma Yingkui <maeikei@gmail.com>
2014-07-02 00:20:43 +0900

    add CephConf

--- interface/web/madoop/app/Controller/CoreOSLiveWebDeployController.php
+++ interface/web/madoop/app/Controller/CoreOSLiveWebDeployController.php
@@ -30,7 +30,7 @@ App::uses('RestFullController', 'Controller');
  * @package		app.Controller
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
-class CephWebDeployController extends RestFullController {
+class CoreOSLiveWebDeployController extends RestFullController {
 	public function index() {
 	}
 	
